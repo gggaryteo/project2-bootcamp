4253cd899df848669ae4d45f3aa2db6bca394161
gggaryteo <garyteoprograms@gmail.com>
2022-12-01 21:48:20 +0800

    initialize firestore

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -18,5 +19,10 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
+// Initialize Firestore
+const db = getFirestore();
+
 // Initialize Cloud Storage and get a reference to the service
 const storage = getStorage(app);
+
+export { db }
