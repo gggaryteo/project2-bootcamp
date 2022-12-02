3681a89f88f532f49f5f4fa699f5d8d1f6b54a11
gggaryteo <garyteoprograms@gmail.com>
2022-12-02 10:58:59 +0800

    Added Navigation Bar Component

--- src/App.js
+++ src/App.js
@@ -10,13 +10,16 @@ import Navbar from "./Components/Navbar";
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route exact path="/" element={<Dashboard/>} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/taskcards/:id" element={<TaskCard />} />
-        <Route path="/create-project" element={<CreateProject/>}/>
-      </Routes>
+      <div className="container">
+        <Navbar/>
+        <Routes>
+          <Route exact path="/" element={<Dashboard />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/taskcards/:id" element={<TaskCard />} />
+          <Route path="/create-project" element={<CreateProject />} />
+        </Routes>
+      </div>
     </div>
   );
 }
