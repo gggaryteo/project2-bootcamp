78673d7083e2d7337503345fb08c627e9b133c33
TheConfusedDuck <lj.michaelhung@gmail.com>
2022-12-05 10:46:28 +0800

    Added signup form, authentication, css

--- src/Pages/Signup/SignupPage.js
+++ src/Pages/Signup/SignupPage.js
@@ -1,9 +1,58 @@
-import "./SignupPage.css"
+import "./SignupPage.css";
+import React, { useState } from "react";
+import { auth } from "../../firebase";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import "./SignupPage.css";
 
 export default function Signup() {
+  const [registerEmail, setRegisterEmail] = useState("");
+  const [registerPassword, setRegisterPassword] = useState("");
+
+  const Create = (event) => {
+    event.preventDefault();
+
+    createUserWithEmailAndPassword(auth, registerEmail, registerPassword)
+      .then((userCredential) => {
+        console.log("Success registeration");
+
+        // if successful, redirect back to login page
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log("Error code: ", errorCode);
+        console.log("Error message: ", errorMessage);
+      });
+  };
+
   return (
-    <main style={{ padding: "1rem 0" }}>
-      <h2>Signup Page</h2>
-    </main>
+    <div className="signupBox">
+      <h2>Sign up Page</h2>
+
+      <form className="formbox">
+        <input
+          className="input"
+          type="text"
+          placeholder="email"
+          onChange={(event) => {
+            setRegisterEmail(event.target.value);
+          }}
+        />
+        <br />
+        <input
+          className="input"
+          type="text"
+          placeholder="password"
+          onChange={(event) => {
+            setRegisterPassword(event.target.value);
+          }}
+        />
+
+        <br />
+        <button className="button" onClick={Create}>
+          Create
+        </button>
+      </form>
+    </div>
   );
 }
