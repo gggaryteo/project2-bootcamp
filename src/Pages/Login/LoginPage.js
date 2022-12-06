fea84e628a12af18ed2a9710007bb4d2c9f1b09f
gggaryteo <garyteoprograms@gmail.com>
2022-12-06 21:48:23 +0800

    added login logic and re-style login form

--- src/Pages/Login/LoginPage.js
+++ src/Pages/Login/LoginPage.js
@@ -1,72 +1,98 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./LoginPage.css";
-import { auth } from "../../firebase";
-import { onAuthStateChanged, signInWithEmailAndPassword } from "firebase/auth";
+
+import { useAuthContext } from "../../hooks/useAuthContext";
+import { auth, db } from "../../firebase";
+import { updateDoc, doc } from "firebase/firestore";
+import { signInWithEmailAndPassword } from "firebase/auth";
 import { Link } from "react-router-dom";
 
 export default function LoginPage() {
-  const [loginEmail, setLoginEmail] = useState("");
-  const [loginPassword, setLoginPassword] = useState("");
-
-  const [user, setUser] = useState({});
-
-  // Track current user
-  onAuthStateChanged(auth, (currentUser) => {
-    setUser(currentUser);
-  });
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [isCancelled, setIsCancelled] = useState(false);
+  const { dispatch } = useAuthContext();
 
   // Login authenticaton
   // Assigned to Login button
   // TO DO: Add navigation logic from login to dashboard
-  const login = (event) => {
+  const handleLogin = async (event) => {
     event.preventDefault();
 
-    signInWithEmailAndPassword(auth, loginEmail, loginPassword)
-      .then((userCredential) => {
-        // Signed in
-        const user = userCredential.user;
-        console.log("Login succesful");
-        console.log(user);
-      })
-      .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        console.log("Error code: ", errorCode);
-        console.log("Error message: ", errorMessage);
+    setError(null);
+    setIsLoading(false);
+
+    try {
+      const response = await signInWithEmailAndPassword(auth, email, password)
+      const user = auth.currentUser;
+      console.log(user);
+
+      if (!response) {
+        throw new Error("Could not complete sign-up");
+      }
+
+      const usersCollectionRef = doc(db, "users", user.uid);
+      await updateDoc(usersCollectionRef, {
+        online: true,
       });
+
+      dispatch({type: 'LOGIN', payload: user})
+
+      if (!isCancelled) {
+        //update states
+        setIsLoading(false);
+        setError(null);
+      }
+
+    } catch(error){
+      if(!isCancelled) {
+        if (error.code === "auth/wrong-password") {
+        setError("Wrong password. Please try again.");
+      } else {
+        setError(error.message);
+      }
+      setIsLoading(false) 
+      }
+    }
   };
 
-  return (
-    <div className="loginbox">
-      <h2 className="header"> Login </h2>
-      <form className="formbox">
-        <input
-          className="input"
-          type="text"
-          placeholder="Enter email"
-          onChange={(event) => {
-            setLoginEmail(event.target.value);
-          }}
-        />
-        <br />
-        <input
-          className="input"
-          type="text"
-          placeholder="Enter password"
-          onChange={(event) => {
-            setLoginPassword(event.target.value);
-          }}
-        />
+  useEffect(() => {
+    return () => setIsCancelled(true);
+  }, []);
 
-        <div>
-          <Link to="/signup">
-            <button className="button">Sign up</button>
-          </Link>
-          <button className="button" onClick={login}>
-            Login
-          </button>
-        </div>
-      </form>
-    </div>
+
+  return (
+    <form className="login-form" onSubmit={handleLogin}>
+      <h2>Login</h2>
+      <label>
+        <span>Enter E-mail: </span>
+        <input
+          required
+          type="email"
+          onChange={(event) => {
+            setEmail(event.target.value);
+          }}
+          value={email}
+        />
+      </label>
+      <label>
+        <span>Enter Password: </span>
+        <input
+          required
+          type="password"
+          onChange={(event) => {
+            setPassword(event.target.value);
+          }}
+          value={password}
+        />
+      </label>
+      
+      {!isLoading && <button className="btn">Login Your Account</button>}
+      {isLoading && (<button className="btn" disabled>Loading</button>
+      )}
+      {error && <div className="error">{error}</div>}
+    </form>
   );
 }
