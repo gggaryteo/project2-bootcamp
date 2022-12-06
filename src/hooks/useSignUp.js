6829bf1f77fa77c2c6de85e2c9b1a198ed85ee55
gggaryteo <garyteoprograms@gmail.com>
2022-12-06 17:39:24 +0800

    added user collection to firestore db

--- src/hooks/useSignUp.js
+++ src/hooks/useSignUp.js
@@ -1,8 +1,9 @@
 import { useState } from "react";
-import { auth, storage } from "../firebase";
+import { auth, storage, db } from "../firebase";
 import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
 import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { useAuthContext } from "./useAuthContext";
+import { collection, setDoc, doc } from "firebase/firestore";
 
 export const useSignUp = () => {
   const [error, setError] = useState(null);
@@ -18,24 +19,33 @@ export const useSignUp = () => {
     try {
       // Create the user
       const response = await createUserWithEmailAndPassword(auth, registerEmail, registerPassword);
-      console.log(response.user)
+      const user = auth.currentUser;
+      console.log(user);
 
       if (!response) {
         throw new Error('Could not complete sign-up');
       }
       // upload user display picture. Store it inside userPictures folder, thereafter, every user has their own folder with a unique ID.
       // Make it easier to iterate through and output the pictures one by one when setting up the User Online Component
-      const uploadPath = `images/${auth.currentUser.uid}/${displayPicture.name}`;
+      const uploadPath = `images/${user.uid}/${displayPicture.name}`;
       const imageRef = ref(storage, uploadPath);
       // Upload image into firebase storage
-      await uploadBytes(imageRef, displayPicture);
+      const img = await uploadBytes(imageRef, displayPicture);
       // Get the Download URL
       const photoURL = await getDownloadURL(imageRef);
 
       // Update the user profile
-      await updateProfile(auth.currentUser, { displayName, photoURL });
+      await updateProfile(user, { displayName, photoURL });
       setIsPending(false);
       setError(null);
+
+      // Create and store user Document in Firestore Database
+      const usersCollectionRef = doc(db, 'users', user.uid)
+      await setDoc(usersCollectionRef, {
+        online: true,
+        displayName,
+        photoURL: photoURL
+      })
 
       // Dispatch Action (useAuthContext)
       dispatch({ type: "LOGIN", payload: auth.currentUser });
