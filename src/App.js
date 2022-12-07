d2bed50da2b02268390a95cf3ebcc26eb6605849
gggaryteo <garyteoprograms@gmail.com>
2022-12-07 11:11:06 +0800

    setup redirects and conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,27 +1,34 @@
 import "./App.css";
 import LoginPage from "./Pages/Login/LoginPage";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Signup from "./Pages/Signup/SignupPage";
 import Dashboard from "./Pages/Dashboard/Dashboard";
 import TaskCard from "./Pages/TaskCard/TaskCard";
 import CreateProject from "./Pages/CreateProject/CreateProject";
 import Navbar from "./Components/Navbar";
 import Sidebar from "./Components/Sidebar";
+import { useAuthContext } from "./hooks/useAuthContext";
 
 function App() {
+  const { user, authIsReady } = useAuthContext();
+
   return (
     <div className="App">
-      <Sidebar/>
-      <div className="container">
-        <Navbar />
-        <Routes>
-          <Route exact path="/" element={<Dashboard />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/taskcards/:id" element={<TaskCard />} />
-          <Route path="/create-project" element={<CreateProject />} />
-        </Routes>
-      </div>
+      {authIsReady && (
+        <>
+          <Sidebar />
+          <div className="container">
+            <Navbar />
+            <Routes>
+              <Route exact path="/" element={user ? <Dashboard /> : <Navigate to="/login"/>} />
+              <Route path="/signup" element={!user ? <Signup /> : <Navigate to="/"/>} />
+              <Route path="/login" element={!user ? <LoginPage /> : <Navigate to="/"/>} />
+              <Route path="/taskcards/:id" element={user ? <TaskCard /> : <Navigate to="/login"/>} />
+              <Route path="/create-project" element={user ? <CreateProject /> : <Navigate to="/login"/>} />
+            </Routes>
+          </div>
+        </>
+      )}
     </div>
   );
 }
--- src/Context/AuthContext.js
+++ src/Context/AuthContext.js
@@ -1,5 +1,7 @@
-import { createContext, useReducer } from "react";
+import { createContext, useEffect, useReducer } from "react";
 import React from "react";
+import { auth } from "../firebase";
+import { onAuthStateChanged } from "firebase/auth";
 
 export const AuthContext = createContext();
 
@@ -12,14 +14,28 @@ export const authReducer = (state, action) => {
     case "LOGOUT":
       return {...state, user: null}
 
+      // show the user content/component tree based off their authentication status in App.js
+    case "AUTH_IS_READY":
+      return {...state, user: action.payload, authIsReady: true}
+
     default:
       return state;
   }
 };
 
 export const AuthContextProvider = (props) => {
-  const [state, dispatch] = useReducer(authReducer, { user: null });
+  const [state, dispatch] = useReducer(authReducer, { user: null, authIsReady: false});
   console.log("Authcontext State:", state);
+
+  // Fire the function once. Communicate to firebase to evaluate whether there is a change in auth status.
+  // If user has logged in, get the user object
+  // If user has logged out, set user to null
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      dispatch({type: 'AUTH_IS_READY', payload: user})
+      unsubscribe();
+    })
+  }, [])
 
   return (
     <AuthContext.Provider value={{ ...state, dispatch }}>
