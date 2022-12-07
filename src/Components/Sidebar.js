25fb789f98ab3297e2a46bb056190458ae931ddf
gggaryteo <garyteoprograms@gmail.com>
2022-12-07 14:45:09 +0800

    Added Sidebar User Avatar

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -4,15 +4,21 @@ import { NavLink } from "react-router-dom";
 import "./Sidebar.css";
 import DashboardIcon from "../assets/dashboard_icon.svg";
 import AddIcon from "../assets/add_icon.svg";
+import UserAvatar from "./UserAvatar";
+import { useAuthContext } from "../hooks/useAuthContext";
 
 
 export default function Sidebar() {
+
+   const { user } = useAuthContext();
+   console.log(user);
+
   return (
     <div className="sidebar">
       <div className="sidebar-content">
         <div className="user">
-          {/* add username here later after storing it in firestore */}
-          <p> Hey User </p>
+          <UserAvatar src={user.photoURL}/> 
+          <p> Sup {user.displayName}! </p>
         </div>
         <nav className="links">
           <ul>
