5b7c808ce3a03dafc7d850952d8d6da94c9de24c
gggaryteo <garyteoprograms@gmail.com>
2022-12-11 20:08:37 +0800

    resize card to fit all content

--- src/Components/ProjectGrid.js
+++ src/Components/ProjectGrid.js
@@ -22,13 +22,19 @@ export default function ProjectGrid({ projects }) {
                 margin: "5px",
                 borderWidth: "2px",
                 transition: "transform 0.15s ease-in-out",
-                  "&:hover": { transform: "scale3d(1.05, 1.05, 1)" },
+                "&:hover": { transform: "scale3d(1.05, 1.05, 1)" },
               }}
+              style={{ height: "100%" }}
             >
               <CardContent>
-                <Link style={{ textDecoration: 'none' }} to={`/taskcards/${taskcard.id}`}>
+                <Link
+                  style={{ textDecoration: "none" }}
+                  to={`/taskcards/${taskcard.id}`}
+                >
                   <p className="p-title">{taskcard.projectName}</p>
-                  <p className="due-date">Due by {taskcard.dueDate.toDate().toLocaleDateString()}</p>
+                  <p className="due-date">
+                    Due by {taskcard.dueDate.toDate().toLocaleDateString()}
+                  </p>
                   <div className="assigned-users">
                     {taskcard.storeAssignedUsers.map((user) => (
                       <UserAvatar key={user.photoURL} src={user.photoURL} />
