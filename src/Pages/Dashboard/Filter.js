7af23d7abbbd49ae9443c2110dd23fcee210f330
gggaryteo <garyteoprograms@gmail.com>
2022-12-11 18:32:02 +0800

    added filter projects component

---
+++ src/Pages/Dashboard/Filter.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import './Dashboard.css'
+
+const INITIAL_ARRAY = ['All', 'Mine', 'Programming', 'DevOps', 'Marketing', 'Pokemon Only']
+
+export default function Filter({ currentFilter, changeFilter }) {
+
+  const handleClick = (filterName) => {
+    console.log(filterName);
+    // Lift state up and call a function in the parent component
+    changeFilter(filterName);
+  }
+
+  return (
+    <div className='project-filter'>
+      <nav>
+        {INITIAL_ARRAY.map((item) => (
+          <button className={currentFilter === item ? "active" : ""} key={item} onClick={() => handleClick(item)}>
+            {item}
+          </button>
+        ))}
+      </nav>
+    </div>
+  )
+}
